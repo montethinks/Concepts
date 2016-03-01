2246e486e0348a346785cd48db608ec0bca32205
Monte Williams <monte.williams@gmail.com>
2016-02-29 21:21:13 -0500

    Finished Array and Hash chapters.

--- 06_arrays/exercise04.rb
+++ 06_arrays/exercise04.rb
@@ -1,10 +1,5 @@
 arr = [15, 7, 18, 5, 12, 8, 5, 1]
 
 arr.index(5)
-# => 3 This is returning the index place of the number passed in as an argument (5).
-
 arr.index[5]
-#does not exist, undefined methohd []
-
-arr[5]
-# => 8
+arr
